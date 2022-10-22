46108b775f78d85455101691029f05507be47864
Dominique Vilain <dominique.vilain@hepl.be>
2022-10-22 08:53:06 +0200

    :sparkles: extract table to regular blade component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,28 +49,8 @@
                     </div>
                 </form>
             </div>
-            <table class="w-full mb-4">
-                <thead class="bg-blue-800 text-white uppercase">
-                    <tr class="[&>th]:py-4 [&>th]:border [&>th]:border-gray-500">
-                        @php
-                            $qpf = array_filter($qp, fn($p) => $p !== 'sort-field' ,ARRAY_FILTER_USE_KEY);
-                        @endphp
-                        <th scope="col" class="tracking-wider"><a href="/?sort-field=name&amp;{{http_build_query($qpf)}}">Name</a></th>
-                        <th scope="col" class="tracking-wider"><a href="/?sort-field=email&amp;{{http_build_query($qpf)}}">Email</a></th>
-                        <th scope="col" class="tracking-wider"><a href="/?sort-field=birthdate&amp;{{http_build_query($qpf)}}">Birthdate</a>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($contacts as $contact)
-                        <tr class="{{$loop->even?'bg-gray-50':'bg-white'}} [&>td]:p-4 [&>td]:border">
-                            <td>{{$contact->name}}</td>
-                            <td>{{$contact->email}}</td>
-                            <td>{{$contact->birthdate->toFormattedDateString()}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+
+            <x-table :qp="$qp" :contacts="$contacts->items()"/>
             {{$contacts->appends($qp)->links('pagination::tailwind')}}
 
         </main>
