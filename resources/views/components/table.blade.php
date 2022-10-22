fa403ea6af485e6516eb23a45e64aef4d481854c
Dominique Vilain <dominique.vilain@hepl.be>
2022-10-22 08:59:33 +0200

    :sparkles: extract table to regular blade component

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -2,7 +2,7 @@
     <thead class="bg-blue-800 text-white uppercase">
         <tr class="[&>th]:py-4 [&>th]:border [&>th]:border-gray-500">
             @php
-                $qpf = array_filter($qp, fn($p) => $p !== 'sort-field' ,ARRAY_FILTER_USE_KEY);
+                $qpf = array_filter($qp, static fn($p) => $p !== 'sort-field' ,ARRAY_FILTER_USE_KEY);
             @endphp
             <th scope="col"
                 class="tracking-wider"><a href="/?sort-field=name&amp;{{http_build_query($qpf)}}">Name</a></th>
